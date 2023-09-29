7ea6a51306a93ee6b3c6211997e94771da446c58
Thijs <thijs@tyz.nl>
2023-09-29 14:34:00 +0200

    Remove query length limit from api
    since this is done in the App.tsc

--- src/api.ts
+++ src/api.ts
@@ -9,40 +9,38 @@ export async function getFlights(
   },
 ): Promise<ReadonlyArray<Flight> | undefined> {
   try {
-    if (query.length >= 3) {
-      // TODO: as the filtering and sorting is done in the client side, the response can be cached so we only lead it once.
-      const results = (await (await fetch("flights.json")).json())
-        .flights as ReadonlyArray<Flight>;
+    // TODO: as the filtering and sorting is done in the client side, the response can be cached so we only lead it once.
+    const results = (await (await fetch("flights.json")).json())
+      .flights as ReadonlyArray<Flight>;
 
-      return results
-        .filter((flight) =>
-          flight.airport.toLowerCase().includes(query.toLowerCase()),
-        )
-        .sort((flight1, flight2) => {
-          switch (options?.sortBy) {
-            case undefined:
-              return 0;
-            case "date": {
-              return (
-                new Date(`${flight1.date} ${flight1.expectedTime}`).getTime() -
-                new Date(`${flight2.date} ${flight2.expectedTime}`).getTime()
-              );
-            }
-            case "airport": {
-              return flight1.airport.localeCompare(flight2.airport);
-            }
-            case "flightNumber": {
-              return flight1.airport.localeCompare(flight2.airport);
-            }
-            default: {
-              throw new Error(`Invalid sortBy option: ${options?.sortBy}`);
-            }
+    return results
+      .filter((flight) =>
+        flight.airport.toLowerCase().includes(query.toLowerCase()),
+      )
+      .sort((flight1, flight2) => {
+        switch (options?.sortBy) {
+          case undefined:
+            return 0;
+          case "date": {
+            return (
+              new Date(`${flight1.date} ${flight1.expectedTime}`).getTime() -
+              new Date(`${flight2.date} ${flight2.expectedTime}`).getTime()
+            );
           }
-        })
-        .sort(() => (options?.sortDirection === "desc" ? -1 : 1))
+          case "airport": {
+            return flight1.airport.localeCompare(flight2.airport);
+          }
+          case "flightNumber": {
+            return flight1.airport.localeCompare(flight2.airport);
+          }
+          default: {
+            throw new Error(`Invalid sortBy option: ${options?.sortBy}`);
+          }
+        }
+      })
+      .sort(() => (options?.sortDirection === "desc" ? -1 : 1))
 
-        .slice(0, options?.limit);
-    }
+      .slice(0, options?.limit);
   } catch (error) {
     console.error(error);
   }
