35fb3d44186897ee36a142c45311173472d21c56
Thijs <thijs@tyz.nl>
2023-10-02 13:50:08 +0200

    App unit test for App
    to test the rendering of the search results

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,34 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { act, render, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
+import { screen } from "@testing-library/dom";
 
-test("renders an input field", () => {
-  const { container } = render(<App />);
+const apiUrl =
+  "http://localhost:3000/schiphol-frontend-assignment/flights.json";
+
+test("App should render an input field", () => {
+  const { container } = render(<App apiUrl={apiUrl} />);
 
   expect(container.querySelector("input")).toBeInTheDocument();
+});
+
+test("App should renders flight results", async () => {
+  act(() => {
+    render(<App apiUrl={apiUrl} />);
+  });
+
+  const input = screen.getByPlaceholderText("query");
+
+  act(() => {
+    userEvent.type(input, "san");
+  });
+  expect(input).toHaveValue("san");
+
+  await waitFor(async () => {
+    expect((await screen.findAllByText("San Francisco")).length).toEqual(2);
+    expect((await screen.findAllByText("Santiago")).length).toEqual(1);
+    expect((await screen.findAllByText("Santiago Com")).length).toEqual(1);
+    expect((await screen.findAllByText("Sandefjord")).length).toEqual(1);
+  });
 });
